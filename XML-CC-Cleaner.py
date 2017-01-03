14d947bae60f0e87eff3e07cca406d946c586afd
Zeerti <skarrj@gmail.com>
2017-01-03 13:22:32 -0700

    Uploaded Correct File
    Uploaded Correct File

--- XML-CC-Cleaner.py
+++ XML-CC-Cleaner.py
@@ -6,16 +6,19 @@ with open(fileName, "r") as file:
 	line = file.readline()
 
 	for line in file:
-		if(line.find("<SwipeData>") != -1):
+		if(line.find("<SwipeData>") != -1): #-1 = Failure
 			bigList.append(line)
 
-		if(line.find("<SubTotAmt>") != -1):
+		if(line.find("<SubTotAmt>") != -1): #-1 = Failure
 			bigList.append(line)
 			
-		if(line.find("<ApprovalCode>") != -1):
+		if(line.find("<ApprovalCode>") != -1): #-1 = Failure
 			bigList.append(line)
-			bigList.append("\n\n\n")
+			bigList.append("\n\n\n") #Approval code is always last to be found (should be at least) this creates a seperation for readability
 
+#Appears to miss the last thing in the bigList() OR is failing to save the last item found into the bigList()
 with open(cleanedFile, "w") as writeFile:
 	for i in range(len(bigList)):
 		writeFile.write(bigList[i])
+
+
