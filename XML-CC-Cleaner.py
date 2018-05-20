6404711f0198130e4727b0299ccd8fbd6fda7130
Garrett <skarrj@gmail.com>
2018-05-19 22:43:31 -0600

    Overhauled.
    Now takes user input for file extension types to search.
    can choose multiple extensions. I.E. "txt, csv, xml etc. etc."
    Searches all file types with specified extensions for keyword.
    Searches with multiple keywords
    No longer outputs to a file, instead prints to console.

--- XML-CC-Cleaner.py
+++ XML-CC-Cleaner.py
@@ -1,74 +1,47 @@
 import glob
 import os
+import pprint
 
 os.chdir(os.curdir) #change directory to current directory where script is located
-fileNames = list() #file list
-bigList = list() #
+fileNames = list() # All discovered files in directory
+bigList = list() # All found matches
+
+# Get Keywords for search and extensions to open
+def _get_UserInput():
+    global extensionKeys
+    global splitKeys
+
+    searchExtension = input("Input all file extensions to open and search. E.G \"xml txt csv\" with spaces as a delimiter: ").lower()
+    extensionKeys = searchExtension.split(' ')
+    searchKey = input("Input keys to search by. Use spaces for multiple keywords(cAsE SeNsiTivE):  ")
+    splitKeys = searchKey.split(' ')
 
 #Get list of Files
 def _get_List_Of_Files():
-	for file in glob.glob("*.txt"):
-		fileNames.append(file)
-		
-	for file in glob.glob("*.log"):
-		fileNames.append(file)
+    for key in extensionKeys:
+        for file in glob.glob("*." + str(key)):
+             fileNames.append(file)
+             
+    ",".join(str(x) for x in fileNames)
+    print(fileNames)
 
-	#Convert list array to string
-	",".join(str(x) for x in fileNames)
+# Search all files in fileNames for key "splitKeys"
+def _find_UserInput():
+    for file in fileNames:
+        print("Opening file: {}".format(file))
+        with open(file, "r") as file:
+            line = file.readline()
 
-
-
-def _find_Credit_Data(currentFile):
-	with open(currentFile, "r") as file:
-		line = file.readline()
-	
-		for line in file:
-			if(line.find("<SwipeData>") != -1): #-1 = Failure
-				bigList.append(line)
-	
-			if(line.find("<SubTotAmt>") != -1): #-1 = Failure
-				bigList.append(line)
-				
-			if(line.find("<ApprovalCode>") != -1): #-1 = Failure
-				bigList.append(line)
-				bigList.append("\n\n\n") #Approval code is always last to be found (should be at least) this creates a seperation for readability
-
-
-#Appears to miss the last thing in the bigList() OR is failing to save the last item found into the bigList()
-def _write_cleaned_to_file(currentFile): 
-	if not bigList: #No hits found.
-		print("UNABLE TO LOCATE SHIT")
-		return 0
-	else:
-
-		print("WRITING TO CURRENT FILE: {}".format(fileNames[currentFile]))
-		cleanedFile = "Updated "+ fileNames[currentFile] #Get last file in list and set updated list
-		with open(cleanedFile, "w") as writeFile: #Write to file and delete entry when completed
-			for o in range(len(bigList), 0, -1):
-				print(o)
-				print("ITEM LIST: {}".format(len(bigList)))
-
-				writeFile.write(bigList[o-1])
-				bigList.pop(o-1)
-				
-	
-	
-
-#####################
-##START SCRIPT HERE##
-#####################
-
-_get_List_Of_Files()#Find all files in current directory and save them
-
-for i in range(len(fileNames), 0, -1): 
-
-	_find_Credit_Data(fileNames[i-1]) #open and search file for matches
-	_write_cleaned_to_file(i-1) #Write newly cleaned files
-	
+            while line:
+                for key in splitKeys:
+                    if(line.find(key) != -1): #-1 = failure
+                        bigList.append(line) # Append entire line for future reference
+                line = file.readline()# Read in next line
+            
 
 
 
-
-
-
-
+_get_UserInput()
+_get_List_Of_Files() #  Find all files in current directory and save them
+_find_UserInput() # 
+pprint.pprint(bigList) # Output all search matches to console
